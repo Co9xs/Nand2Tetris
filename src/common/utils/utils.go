1f6e4540ac6fe5373dec49af9c4b951ee1a14d27
Co9xs <ryo.fjsm0203@icloud.com>
2021-08-21 11:08:07 +0900

    feat: add util func

--- src/common/utils/utils.go
+++ src/common/utils/utils.go
@@ -1,7 +1,31 @@
 package utils
 
+import (
+	"fmt"
+	"strings"
+)
+
 func HandleErr(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func LogDone(a string, b string) {
+	fmt.Printf("- %-30s -> %-30s \xE2\x9C\x94 done\n", a, b)
+}
+
+func filterNewLines(s string) string {
+	return strings.Map(func(r rune) rune {
+		switch r {
+		case 0x000A, 0x000B, 0x000C, 0x000D, 0x0085, 0x2028, 0x2029:
+			return -1
+		default:
+			return r
+		}
+	}, s)
+}
+
+func CompareStrings(a, b string) bool {
+	return filterNewLines(a) == filterNewLines(b)
 }
